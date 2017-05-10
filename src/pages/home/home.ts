c923afdb9a812504c84ea3b539c025cce7f9381a
Shatilla Prayer <shatilla.prayer@kaseya.com>
2017-05-10 09:41:15 -0700

    init mobile ionic app

---
+++ src/pages/home/home.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  locationName: any;
+
+  constructor(public navCtrl: NavController, public http: Http) {
+    this.http.get('http://api.apixu.com/v1/current.json?key=74d49cd76a79430cad0214409170805&q=89110').map(res => res.json()).subscribe(data => {
+        this.locationName = data.location.name;
+    });
+  }
+
+}
